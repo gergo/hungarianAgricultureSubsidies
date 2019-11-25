a2b1be63c05e102cdd323d4244625528b045d032
szekelygergo <szekelygergoo@gmail.com>
2019-11-25 22:51:02 +0100

    fix dir for csv convert script

--- scripts/change_encoding.sh
+++ scripts/change_encoding.sh
@@ -4,8 +4,10 @@ FROM_ENCODING="ISO-8859-2"
 TO_ENCODING="UTF-8"
 
 CONVERT=" iconv -f $FROM_ENCODING -t $TO_ENCODING"
+cd `pwd`/../input/csv/
 
-for file in ../input/csv/*.csv; do
+for file in *.csv; do
+    echo converting "$file"
     $CONVERT "$file" > "utf8_${file}"
 done
 exit 0
